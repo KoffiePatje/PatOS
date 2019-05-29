313b3bb951444806f1569de16bbda3cd0275ce75
Patrick le Duc <koffiepatje@gmail.com>
2019-05-29 14:37:34 +0200

    First version of Prefs

--- inc/Prefs.lua
+++ inc/Prefs.lua
@@ -1,6 +1,32 @@
+local preferencePath = 'PatOS/saves/%s/preferences.save'
+local t = {}
 
-local table
+function set(name, value)
+	if not t[name] then 
+		t.insert(name, value)
+	else
+		t[name] = value
+	end
+end
 
-function set(name, value) {
+function get(name, value)
+	return t[name]
+end
+
+function save(name)
+	local savePath = preferencePath.format(name)
+	local file = fs.open(savePath, 'w')
+	file.write(textutils.serialize(t))
+	file.close()
+end
+
+function load(name) {
+	local loadPath = preferencePath.format(name)
+	local file = fs.open(loadPath, 'r')
+	local fileContent = file.readAll()
+	file.close()
 	
+	t = textutils.unserialize(fileContent)
 }
+
+load()
